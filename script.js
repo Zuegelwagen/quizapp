24ad39c8f01971aa511eb51664a155c64b306d23
Martin Inglin <m.inglin@gmx.ch>
2023-09-10 21:57:16 +0200

    array created

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        question: "Was versteht ein Tontechniker unter einer DAW?",
+        answer_1: "Digital Audio Workstation",
+        answer_2: "Dominant Audio Wave",
+        answer_3: "Do Audio Worse",
+        answer_4: "Dont also work",
+        right_answer: 1
+    },
+    {
+        question: "Welchen Effekt kennt der Tontechniker?",
+        answer_1: "Fernwartungseffekt",
+        answer_2: "Überwachungseffekt",
+        answer_3: "No blame Effekt",
+        answer_4: "Nahbesprechungseffekt",
+        right_answer: 4
+    },
+    {
+        question: "Wer ist der mühsamste Musiker im Studio?",
+        answer_1: "Gitarrist",
+        answer_2: "Sänger",
+        answer_3: "Drummer",
+        answer_4: "Alle Musiker sind mühsam",
+        right_answer: 4
+    },
+    {
+        question: "Wer hat auf Video killed the radio star den Synthesizer gespielt?",
+        answer_1: "Michael Jackson",
+        answer_2: "Hans Zimmer",
+        answer_3: "Freddy Mercury",
+        answer_4: "Anne-Sophie Mutter",
+        right_answer: 2
+    },
+    {
+        question: "Wie viele Saiten hat eine Gitarre?",
+        answer_1: "1",
+        answer_2: "2",
+        answer_3: "4",
+        answer_4: "6",
+        right_answer: 4
+    },
+]
